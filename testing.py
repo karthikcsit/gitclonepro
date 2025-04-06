bb6c6a8c3d1eafbe328cebbd1353d545e7b8265a
Karthik C S <cs7662988@gmail.com>
2025-04-06 12:51:40 +0530

    Create testing.py

---
+++ testing.py
@@ -0,0 +1,15 @@
+def main():
+    print("hi")
+    numbers = [1,2,3,4,5,6,7,8,9]
+    for num in numbers:
+        print(num)
+
+    print("hi")
+    print("inside test")
+    print("inside main")
+
+
+
+
+if __name__ == "__main__":
+    main()
